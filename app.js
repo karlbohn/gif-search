3d4bb2b107134ad292acca0e7cdf994a6829f0c5
Karl Bohn <karl@McBain>
2023-11-07 12:16:36 -0500

    Removed comments and erroneous code

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-console.log("Let's get this party started!");
-
-
-
 function newGif(response){
 
     let numResults = response.data.length;
@@ -16,7 +12,6 @@ function newGif(response){
 
 $('form').on("submit", async function(e){
     e.preventDefault();
-    //create img, set source to searchGif() response, append to body
     let $searchTerm = $('input').val()
     let response = await axios.get('http://api.giphy.com/v1/gifs/search', { params : {
         api_key : 'wQU5a1KBrWTTy732dEaSpqUydu01baaQ',
@@ -29,6 +24,3 @@ $('form').on("submit", async function(e){
 $('#delete').on("click", function(){
     $('div').empty();
 })
-//GIF URL - api.giphy.com/v1/gifs/search
-// Params- api_key: string(required) || q: string(required)
-//API Key - wQU5a1KBrWTTy732dEaSpqUydu01baaQ
